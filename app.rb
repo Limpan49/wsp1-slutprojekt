94509822b361847933cf3974d1f3fc3c19165b61
Limpan49 <limparlimpan@gmail.com>
2026-03-05 16:20:47 +0100

    .

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'debug'
 require "awesome_print"
 
 class App < Sinatra::Base
+  enable :session
 
     setup_development_features(self)
 
@@ -72,33 +73,30 @@ class App < Sinatra::Base
     get '/categories/:id/threads/new' do |id|
       @category_id = id
       erb :"threads/new"
+    end 
+
+    post '/threads' do
+      redirect '/login' unless logged_in?
+    
+      db.execute(
+        'INSERT INTO threads (title, category_id, user_id) VALUES (?, ?, ?)', 
+        [params["title"], params["category_id"], current_user["id"]]
+      )
+    
+      redirect "/categories/#{params["category_id"]}"
     end
 
 
-    post '/threads' do 
-      title = params["title"]
-      category_id = params["category_id"]
-
-      db.execute(
-        'INSERT INTO threads (title, category_id) VALUES (?, ?)', 
-        [title, category_id]
-      )
-
-      redirect "/categories/#{category_id}"
-    end 
-
-
-
-    post '/threads/:id/posts' do id 
-      content = params["content"]
-
+    post '/threads/:id/posts' do |id|
+      redirect '/login' unless logged_in?
+    
       db.execute(
         'INSERT INTO posts (content, user_id, thread_id) VALUES (?, ?, ?)',
-        [content, 1, id]
+        [params["content"], current_user["id"], id]
       )
+    
       redirect "/threads/#{id}"
-    end 
-
+    end
 
     get '/registrera' do
       erb :"users/registrera"
@@ -117,5 +115,36 @@ class App < Sinatra::Base
 
       redirect '/login'
     end 
+
+
+
+
+
+
+    get '/login' do
+      erb :"users/login"
+    end
+    
+    post '/login' do
+      require 'bcrypt'
+    
+      user = db.execute(
+        "SELECT * FROM users WHERE username = ?",
+        [params[:username]]
+      ).first
+    
+      if user && BCrypt::Password.new(user["password_digest"]) == params[:password]
+        session[:user_id] = user["id"]
+        redirect '/'
+      else
+        @error = "Fel användarnamn eller lösenord"
+        erb :"users/login"
+      end
+    end
+    
+    get '/logout' do
+      session.clear
+      redirect '/'
+    end
 
 end
